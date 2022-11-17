a93d9cbf0124a84d694f09c83e5babe5807bc953
Robson santos <rcflorestal@yahoo.com.br>
2022-11-17 18:07:21 -0300

    R script

---
+++ sistaxonQuery.R
@@ -0,0 +1,23 @@
+library(dplyr, warn.conflicts = FALSE)
+
+## rad dataset
+sistaxon <- read.csv2('./2021-04-30-sistaxon_DOF-Especies-e-Codigos.csv')
+
+## Query by popular name
+sistaxon_seek <- sistaxon %>%
+        filter(NomePopular == 'Acapu' | NomePopular == 'Acapurana')
+
+## Query by genus
+genus <- sistaxon %>%
+        filter(grepl('^Vouacapoua', NomeCientifico))
+
+## Save query as csv file
+write.csv2(sistaxon_seek, 
+           './output/psidiumREFLORA.csv', 
+           row.names = FALSE)
+
+CITES_443 <- read.csv2('D:/data/endangeredBrazilianPlantSpecies/output/Especies_Ameacadas_BRA.csv', fileEncoding = 'latin1')
+
+
+sistaxon_seek %>%
+        inner_join(CITES_443, by = c('NomeCientifico' = 'nome_cientifico'))
