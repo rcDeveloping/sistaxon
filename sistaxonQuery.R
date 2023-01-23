9a193cb5e315ce833660fdf7e99043b629fe4dd3
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2023-01-23 12:39:22 -0300

    Query by specie

--- sistaxonQuery.R
+++ sistaxonQuery.R
@@ -1,23 +1,31 @@
 library(dplyr, warn.conflicts = FALSE)
 
 ## rad dataset
-sistaxon <- read.csv2('./data/2021-04-30-sistaxon_DOF-Especies-e-Codigos.csv', fileEncoding = 'latin1')
+sistaxon <- read.csv2('./sistaxon/data/2021-04-30-sistaxon_DOF-Especies-e-Codigos.csv', 
+                      fileEncoding = 'latin1')
 
 ## Query by popular name
 sistaxon_seek <- sistaxon %>%
-        filter(NomePopular == 'Acapu' | NomePopular == 'Acapurana')
+        filter(NomePopular == 'Inharé')
 
 ## Query by genus
 genus <- sistaxon %>%
         filter(grepl('^Vouacapoua', NomeCientifico))
+
+
+## Query by specie
+sp <- sistaxon %>%
+        filter(grepl('Handroanthus spongiosus', NomeCientifico))
 
 ## Save query as csv file
 write.csv2(sistaxon_seek, 
            './output/psidiumREFLORA.csv', 
            row.names = FALSE)
 
-CITES_443 <- read.csv2('D:/data/endangeredBrazilianPlantSpecies/output/Especies_Ameacadas_BRA.csv', fileEncoding = 'latin1')
+CITES_443 <- read.csv2('D:/data/endangeredBrazilianPlantSpecies/output/Especies_Ameacadas_BRA.csv', 
+                       fileEncoding = 'latin1')
 
 
 sistaxon_seek %>%
-        inner_join(CITES_443, by = c('NomeCientifico' = 'nome_cientifico'))
+        inner_join(CITES_443, 
+                   by = c('NomeCientifico' = 'nome_cientifico'))
