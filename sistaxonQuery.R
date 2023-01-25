aa8385a7da0d43b09ea62c042019201e646c06d3
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2023-01-25 16:23:23 -0300

    Query in SISTAXON

--- sistaxonQuery.R
+++ sistaxonQuery.R
@@ -1,31 +1,39 @@
 library(dplyr, warn.conflicts = FALSE)
 
-## rad dataset
-sistaxon <- read.csv2('./sistaxon/data/2021-04-30-sistaxon_DOF-Especies-e-Codigos.csv', 
-                      fileEncoding = 'latin1')
+## read data sets
+sistaxon <- read.csv2(
+        './data/2021-04-30-sistaxon_DOF-Especies-e-Codigos.csv', fileEncoding = 'latin1'
+)
+
+CITES_443 <- read.csv2(
+        './data/Especies_Ameacadas_BRA.csv', 
+        fileEncoding = 'latin1'
+)
 
 ## Query by popular name
 sistaxon_seek <- sistaxon %>%
-        filter(NomePopular == 'Inharé')
+        filter(NomePopular == 'Acapu')
+
+sistaxon_seek
 
 ## Query by genus
 genus <- sistaxon %>%
         filter(grepl('^Vouacapoua', NomeCientifico))
 
+genus
 
 ## Query by specie
 sp <- sistaxon %>%
         filter(grepl('Handroanthus spongiosus', NomeCientifico))
 
+sp 
+
 ## Save query as csv file
 write.csv2(sistaxon_seek, 
-           './output/psidiumREFLORA.csv', 
+           './output/Vouacapoua.csv', 
            row.names = FALSE)
 
-CITES_443 <- read.csv2('D:/data/endangeredBrazilianPlantSpecies/output/Especies_Ameacadas_BRA.csv', 
-                       fileEncoding = 'latin1')
-
-
+## Find out the ecological status of the specie
 sistaxon_seek %>%
         inner_join(CITES_443, 
                    by = c('NomeCientifico' = 'nome_cientifico'))
