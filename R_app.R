437c29a01f93466f2d842153e8ac1e6fefbc7460
Robson Cruz Santos <rcflorestal@yahoo.com.br>
2023-02-08 13:49:34 -0300

    Define theme

--- R_app.R
+++ R_app.R
@@ -1,14 +1,24 @@
 library(shiny)
 
+
+url <- 'http://www.ibama.gov.br/phocadownload/sinaflor/2022/2022-07-22_Lista_especies_DOF.csv'
+
+con <- read.csv(url, fileEncoding = 'latin1')
+sistaxon <- con
+rm(con)
+names(sistaxon) <- c('Código da espécie', 'Nome científico', 
+                     'Código Nome Popular', 'Nome popular')
+
 ui <- fluidPage(
         titlePanel('SISTAXON'),
+        theme = shinythemes::shinytheme('superhero'),
         textInput('name', 'Enter your name:'),
         tableOutput('out')
 )
 
 server <- function(input, output, session) {
         output$out <- renderTable({
-                subset(sistaxon, NomeCientifico == R.utils::capitalize(tolower(input$name))) 
+                subset(sistaxon, `Nome científico` == R.utils::capitalize(tolower(input$name))) 
         })
         
 }
